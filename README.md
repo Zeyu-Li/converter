c7bab0603ecf51646d13b280a74ee72483f7a0d2
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-02 15:40:07 -0600

    marked up code and demo

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Converter
 
-An app that can convert measurements ie from teaspoon to table spoon
+An app that can convert measurements ie from teaspoon to table spoon. Has two options, wet and dry parts
 
+![screenshot](img/screenshot.png)
 
+Click on the blue buttons to cycle through different items. Click Reset to set values back to nothing
 
 ## Use
 
@@ -17,4 +19,7 @@ To start emulation, use `expo start`
 * View = div
 * Text = p
 * styles can be mounted with a `StyleSheet.create`
-* 
+* TextInput = input
+* Image = img
+* TouchableOpacity = a div that can trigger something via click
+
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,17 +2,22 @@ import React, {Component, useEffect, useState} from 'react';
 import { StyleSheet, Text, TextInput, View, Image, Dimensions, Button, Switch, TouchableOpacity  } from 'react-native';
 import convert from 'convert-units'
 
+// TODO: select picker with npm install @react-native-picker/picker --save
+
 const Converter = () => {
     // default values
     const [dry, setDry] = useState(true);
     const defaultState = [1,3];
     const [firstValue, value1] = useState(defaultState[0]);
     const [secondValue, value2] = useState(defaultState[1]);
+
+    // items and there translations
     const itemsDry = ['mg', 'g', 'kg', 'oz', 'lb'];
-    const itemsDryTranslation = {'mg':'milligram', 'g': 'gram', 'kg': 'kilogram', 'oz':'ounce', 'lb':'pound'};
     const itemsWet = ['ml', 'l', 'tsp', 'Tbs', 'fl-oz', 'cup', 'pnt', 'qt', 'gal'];
+    const itemsDryTranslation = {'mg':'milligram', 'g': 'gram', 'kg': 'kilogram', 'oz':'ounce', 'lb':'pound'};
     const itemsWetTranslation = {'ml':'milliliter', 'l': 'liter', 'tsp':'teaspoon', 'Tbs':'tablespoon', 'fl-oz':'fluid-ounce', 'cup':'cup', 'pnt':'pint', 'qt':'quart', 'gal':'gallon'};
 
+    // decide name depending on where it is dry or wet
     const name1 = () => {
         let itemName = dry ? itemsDryTranslation[itemsDry[firstValue]] : itemsWetTranslation[itemsWet[firstValue]];
         return itemName;
@@ -28,15 +33,18 @@ const Converter = () => {
 
     // bidirectional is tricky
     useEffect(()=> {
+        // base case, ie constructor therefore don't do anything
         if (num1 === '') return;
         let value = dry ? convert(num1).from(itemsDry[firstValue]).to(itemsDry[secondValue]) : convert(num1).from(itemsWet[firstValue]).to(itemsWet[secondValue]);
         // in case of int
         try {if (Math.round(value) !== value) value=value.toFixed(3);} catch {;}
         changeNum2(value);
     }, [num1, firstValue, secondValue])
+    // if these values change ^^^, do the effect (acts like event listeners)
 
     return (
         <View style={styles.convert}>
+            {/* dry/wet switch */}
             <View style={styles.masterSwitch}>
                 <Text style={styles.regularText}>Wet</Text>
                 <Switch
@@ -50,6 +58,7 @@ const Converter = () => {
                 /> 
                 <Text style={styles.regularText}>Dry</Text>
             </View>
+            {/* first button */}
             <View style={[styles.button]}>
                 <Button title={`Change from `+ name1()} onPress={() => {
                     // changes input value
@@ -58,6 +67,7 @@ const Converter = () => {
                     value1(c => c+change)
                 }}/>
             </View>
+            {/* input field */}
             <TextInput keyboardType={'numeric'} placeholder={name1()} style={[styles.containerInput]} value={num1} onChange={e => changeNum1(e.target.value)} />
             {/* touch image to switch conversion type */}
             <TouchableOpacity onPress={()=>{
@@ -67,7 +77,9 @@ const Converter = () => {
             }}>
                 <Image style={[styles.image]} source={require('@expo/../../assets/img/switch.svg')} />
             </TouchableOpacity>
+            {/* output field, locked/uneditable */}
             <TextInput editable={false} keyboardType={'numeric'} placeholder={name2()} style={[styles.containerInput]} value={num2} onChange={e => changeNum2(e.target.value)} />
+            {/* second button */}
             <View style={[styles.button]}>
                 <Button title={`Change from `+ name2()} style={[styles.button]} onPress={() => {
                     // changes input value
